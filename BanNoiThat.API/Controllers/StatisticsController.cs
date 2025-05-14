8490babc154c84a6d4b44b5484f655e7485114b6
ACER <ACER@DESKTOP-7FU0PLQ>
2025-05-14 22:25:22 +0700

    Analyst API

--- BanNoiThat.API/Controllers/StatisticsController.cs
+++ BanNoiThat.API/Controllers/StatisticsController.cs
@@ -1,6 +1,11 @@
-﻿using BanNoiThat.API.Model;
+﻿using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+using BanNoiThat.API.Model;
 using BanNoiThat.Application.Interfaces.IService;
+using BanNoiThat.Application.Interfaces.Repository;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using Newtonsoft.Json.Linq;
 
 namespace BanNoiThat.API.Controllers
 {
@@ -9,33 +14,88 @@ namespace BanNoiThat.API.Controllers
     public class StatisticsController : ControllerBase
     {
         private readonly IStatisticService _statisticsService;
+        private readonly IUnitOfWork _uow;
+        private readonly ApiResponse _apiReponse;
 
-        public StatisticsController(IStatisticService statisticsService)
+        public StatisticsController(IStatisticService statisticsService, IUnitOfWork uow)
         {
             _statisticsService = statisticsService;
+            _apiReponse = new ApiResponse();
+            _uow = uow;
         }
 
 
         [HttpGet("revenue")]
-        public IActionResult GetRevenue()
+        public async Task<IActionResult> GetRevenue()
         {
-            var revenue = _statisticsService.GetRevenue();  // Thống kê doanh thu
-            return Ok(revenue);
+            var daynow = DateTime.Today;
+            var firstDayLastWeek = DateTime.Today - new TimeSpan((int)daynow.DayOfWeek + 7 - 1, 0, 0, 0);
+            var firstDayWeek = DateTime.Today - new TimeSpan((int)daynow.DayOfWeek - 1, 0, 0, 0);
+
+            var listEntity = await _uow.OrderRepository.GetAllAsync(x => x.OrderPaidTime >= firstDayLastWeek);
+            double revenueLastWeek = 0;
+            double revenueWeek = 0;
+
+            foreach (var item in listEntity)
+            {
+                if (item.OrderPaidTime >= firstDayWeek)
+                    revenueLastWeek += item.TotalPrice;
+                else
+                    revenueWeek += item.TotalPrice;
+            }
+
+            _apiReponse.Result = new
+            {
+                revenueLastWeek = revenueLastWeek,
+                revenueWeek = revenueWeek,
+            };
+
+            return Ok(_apiReponse);
         }
 
         [HttpGet("orders")]
-        public IActionResult GetNumberOfOrders()
+        public async Task<ActionResult<ApiResponse>> GetNumberOfOrders()
         {
-            var products = _statisticsService.GetNumberOfOrders(); 
-            return Ok(products);
+            var daynow = DateTime.Today;
+            var firstDayLastWeek = DateTime.Today - new TimeSpan((int)daynow.DayOfWeek + 7 - 1, 0, 0, 0);
+            var firstDayWeek = DateTime.Today - new TimeSpan((int)daynow.DayOfWeek - 1, 0, 0, 0);
+
+            var listEntity = await _uow.OrderRepository.GetAllAsync(x => x.OrderPaidTime >= firstDayLastWeek);
+
+            var entityWeek = listEntity.Where(x => x.OrderPaidTime >= firstDayWeek).Count();
+            var entityLastWeek = listEntity.Where(x => x.OrderPaidTime < firstDayWeek).Count();
+
+            _apiReponse.Result = new
+            {
+                NumberLastWeek = entityLastWeek,
+                NumberWeek = entityWeek,
+            };
+
+            _apiReponse.IsSuccess = true;
+
+            return Ok(_apiReponse);
         }
 
-        [HttpGet("users")]
-        public IActionResult Get()
+        [HttpGet("chart")]
+        public async Task<ActionResult<ApiResponse>> GetRevenueChart([FromQuery] int year)
         {
-            var products = _statisticsService.GetNumberOfUser();
-            return Ok(products);
+            var orders = await _uow.OrderRepository.GetAllAsync(x => x.OrderPaidTime.Year == year);
+            var arrRevenueOfYear = new double[12];
+
+            foreach(var order in orders)
+            {
+                arrRevenueOfYear[order.OrderPaidTime.Month] += order.TotalPrice;
+            }
+
+            _apiReponse.Result = new
+            {
+                nameLabel = "Revenue" + year,
+                x = arrRevenueOfYear,
+            };
+
+            _apiReponse.IsSuccess = true;
+
+            return Ok(_apiReponse);
         }
     }
-
 }
