541ea4e0d4f769691cea0f191cfc2358abd80b23
Tran Tuan Anh <tuananh2003.it@gmail.com>
2025-05-29 22:43:03 +0700

    get size product item

--- BanNoiThat.Application/DTOs/ProductDtos/ProductResponse.cs
+++ BanNoiThat.Application/DTOs/ProductDtos/ProductResponse.cs
@@ -12,6 +12,6 @@ namespace BanNoiThat.Application.DTOs.ProductDtos
         public string Name { get; set; }
         public string Description { get; set; }
         public string ThumbnailUrl { get; set; }
-        public string Slug { get; set; }
+        public string Slug { get; set; } 
     }
 }
