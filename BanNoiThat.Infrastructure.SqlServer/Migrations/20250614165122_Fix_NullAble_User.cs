cc0e373d11f69f03f9cfed922fc5373ac115d021
Tran Tuan Anh <tuananh2003.it@gmail.com>
2025-06-14 23:52:50 +0700

    Fix null user

---
+++ BanNoiThat.Infrastructure.SqlServer/Migrations/20250614165122_Fix_NullAble_User.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BanNoiThat.Infrastructure.SqlServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_NullAble_User : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsBlocked",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false,
+                oldClrType: typeof(bool),
+                oldType: "bit",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<bool>(
+                name: "IsBlocked",
+                table: "Users",
+                type: "bit",
+                nullable: true,
+                oldClrType: typeof(bool),
+                oldType: "bit");
+        }
+    }
+}
