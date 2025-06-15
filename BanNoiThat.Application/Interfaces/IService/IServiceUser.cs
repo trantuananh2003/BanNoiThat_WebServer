3bf03541fc15093bcb0347ae31d84e2b70b1a993
Tran Tuan Anh <tuananh2003.it@gmail.com>
2025-06-15 17:14:06 +0700

    fix Error

--- BanNoiThat.Application/Interfaces/IService/IServiceUser.cs
+++ BanNoiThat.Application/Interfaces/IService/IServiceUser.cs
@@ -5,7 +5,7 @@ namespace BanNoiThat.Application.Interfaces.IService
 {
     public interface IServiceUser
     {
-        Task<PagedList<UnitUserMangeReponse>> GetAllUser();
+        Task<List<UnitUserMangeReponse>> GetAllUser();
         Task<InfoUserResponse> GetInfoUser(string userId);
         Task UpdateInfoUser(string userId, InfoUserRequest modelRequest);
         Task UpdateUserBlock(string userId, bool isBlock);
